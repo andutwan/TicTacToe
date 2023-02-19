276f2ede2937d90b893b200d60348b23b45c5d90
andutwan <andrewtran2638@gmail.com>
2023-02-19 14:57:49 -0800

    correct print function, add comments

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,29 @@
+# Function to print out the board
+def printBoard(board):
+    # Iterate through the rows
+    for row in board:
+        # Print out the values in the curren row
+        for i in range(3):
+            # Not end of the row so print same line
+            if i < 2:
+                print(row[i] + " ", end = " ")
+            else:
+                # Last element print character and newline
+                print(row[i])
 
+# Function performs game
+def playGame(board):
+    # Print the inital board
+    printBoard(board)
+
+    
+
+def main():
+    # Creat our board
+    board = [["*", "*", "*"],
+             ["*", "*", "*"],
+             ["*", "*", "*"]]
+    # Start our game
+    playGame(board)
+
+main()
